6ed2d3a97213d73fd8bc83f7b373ddc8bf390cca
KHaaN007 <khaanmr007@gmail.com>
2023-11-06 04:58:11 +0600

    Create Post for Add Job Data From Client Side

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2jxlghj.mongodb.net/?retryWrites=true&w=majority`;
 
+
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,8 +30,26 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
+
+    const jobCollection = client.db("jobsDB").collection("job")
+
+
+
+   /**Connect the client to the server**/ /**ADD job**/
+   app.post('/job', async (req, res) => {
+    // delete req.body._id
+    const newjob = req.body;
+    const result = await jobCollection.insertOne(newjob)
+    res.send(result)
+})
+
+
+
+
+
+
 
 
 
@@ -39,7 +59,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
