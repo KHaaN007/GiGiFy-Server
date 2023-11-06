ae539570fa6b9739543425084460f51379f9d80c
KHaaN007 <khaanmr007@gmail.com>
2023-11-06 22:26:39 +0600

    Get Data From DataBase For Job Detail

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2jxlghj.mongodb.net/?retryWrites=true&w=majority`;
 
 
@@ -56,6 +56,12 @@ async function run() {
         })
 
 
+        app.get('/jobDetail/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await jobCollection.findOne(query)
+            res.send(result)
+        })
 
 
 
